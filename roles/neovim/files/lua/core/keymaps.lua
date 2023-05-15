016e6c94629b8e59f541f51cbaf8bff1287edc28
Frederico Costa <1683633+bzook@users.noreply.github.com>
2023-05-16 00:14:55 +0100

    neovim additional plugins

--- roles/neovim/files/lua/core/keymaps.lua
+++ roles/neovim/files/lua/core/keymaps.lua
@@ -6,3 +6,16 @@ vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
 
 vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
 
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("n", "J", "mzJ`z")
+
+-- greatest remap ever
+vim.keymap.set("x", "<leader>p", [["_dP]])
+
+-- next greatest remap ever : asbjornHaland
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
