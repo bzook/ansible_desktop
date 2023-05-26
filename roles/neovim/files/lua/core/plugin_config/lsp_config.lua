b792aaa2602c20388586cff2bd37b384871b497e
Frederico Costa <1683633+bzook@users.noreply.github.com>
2023-05-26 09:31:55 +0100

    several tweaks to unused config and support for php lsp

--- roles/neovim/files/lua/core/plugin_config/lsp_config.lua
+++ roles/neovim/files/lua/core/plugin_config/lsp_config.lua
@@ -3,22 +3,6 @@ require("mason-lspconfig").setup()
 local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 local opts = { noremap = true, silent = true }
 
-require('lspsaga').setup({
-  code_action_icon = "💡",
-  symbol_in_winbar = {
-    in_custom = false,
-    enable = true,
-    separator = ' ',
-    show_file = true,
-    file_formatter = ""
-  },
-})
-
-vim.keymap.set("n", "gd", "<cmd>Lspsaga lsp_finder<CR>", { silent = true })
-vim.keymap.set('n', 'K', '<Cmd>Lspsaga hover_doc<cr>', { silent = true })
-vim.keymap.set({"n","v"}, "<leader>ca", "<cmd>Lspsaga code_action<CR>", { silent = true })
-vim.keymap.set("n", "<leader>rn", "<cmd>Lspsaga rename<CR>", { silent = true })
-
 require("lspconfig").lua_ls.setup {
   capabilities = capabilities,
   settings = {
@@ -41,10 +25,24 @@ require("lspconfig").intelephense.setup({
   on_attach = function(_, bufnr)
     -- Enable (omnifunc) completion triggered by <c-x><c-o>
     vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
-    vim.api.nvim_buf_set_keymap(bufnr, "n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
     -- Here we should add additional keymaps and configuration options.
   end,
   flags = {
     debounce_text_changes = 150,
-  }
+  },
+  settings = {
+    stubs = {
+      "apache", "bcmath", "bz2", "calendar", "com_dotnet", "Core", "ctype", "curl", "date",
+      "dba", "dom", "enchant", "exif", "FFI", "fileinfo", "filter", "fpm", "ftp", "gd", "gettext",
+      "gmp", "hash", "iconv", "imap", "intl", "json", "ldap", "libxml", "mbstring", "meta", "mysqli",
+      "oci8", "odbc", "openssl", "pcntl", "pcre", "PDO", "pdo_ibm", "pdo_mysql", "pdo_pgsql", "pdo_sqlite", "pgsql",
+      "Phar", "posix", "pspell", "readline", "Reflection", "session", "shmop", "SimpleXML", "snmp", "soap",
+      "sockets", "sodium", "SPL", "sqlite3", "standard", "superglobals", "sysvmsg", "sysvsem", "sysvshm", "tidy",
+      "tokenizer", "xml", "xmlreader", "xmlrpc", "xmlwriter", "xsl", "Zend OPcache", "zip", "zlib",
+      "wordpress", "phpunit", "xdebug"
+    },
+  },
+  diagnostics = {
+    enable = true,
+  },
 })
